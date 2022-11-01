9d4dc8cb499ea7f208af45f0fab9999c421fecef
modhi-17 <85395969+modhi-17@users.noreply.github.com>
2022-11-01 14:13:30 +0100

    quatrième commit

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-'use strict';
+"use strict";
+let changePicture = document.getElementById("image");
+let changeButton = document.getElementById("change");
+console.log(changeButton);
+//console.log(changePicture);
+
+function onChangePicture() {
+  if (changePicture.src === "./images/jet ski 2.png") {
+    changePicture.src = "./images/jet ski 1.png";
+  } else if (changePicture.src = "./images/jet ski 1.png") {
+    changePicture.src = "./images/jet ski 2.png";
+  }
+}
+changeButton.addEventListener("click", onChangePicture);
